0e1ed2574d71caa719d735f53f894b6687fe7974
Brandon <al214858@alumnos.uacj.mx>
2024-02-21 12:20:49 -0700

    Updated home view

--- src/main/java/com/mycompany/youtube/Home.java
+++ src/main/java/com/mycompany/youtube/Home.java
@@ -26,74 +26,179 @@ public class Home extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel2 = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
+        background = new javax.swing.JPanel();
+        bg_cabecera = new javax.swing.JPanel();
+        busqueda_history = new javax.swing.JTextField();
+        icon_logo = new javax.swing.JLabel();
+        label_title = new javax.swing.JLabel();
+        bg_lateral_view = new javax.swing.JPanel();
+        label_inicio = new javax.swing.JLabel();
+        label_inicio1 = new javax.swing.JLabel();
+        label_inicio2 = new javax.swing.JLabel();
+        label_inicio3 = new javax.swing.JLabel();
+        icon_home_bl = new javax.swing.JLabel();
+        icon_suscrip_bl = new javax.swing.JLabel();
+        icon_history_bl = new javax.swing.JLabel();
+        icon_like_bl = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        icon_arrow_bl = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1080, 720));
+        setMinimumSize(new java.awt.Dimension(1080, 720));
+        setPreferredSize(new java.awt.Dimension(1080, 720));
+        setSize(new java.awt.Dimension(1080, 720));
 
-        jPanel2.setBackground(new java.awt.Color(2, 30, 26));
+        background.setBackground(new java.awt.Color(2, 30, 26));
+        background.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 882, Short.MAX_VALUE)
+        bg_cabecera.setBackground(new java.awt.Color(6, 137, 137));
+
+        busqueda_history.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                busqueda_historyActionPerformed(evt);
+            }
+        });
+
+        icon_logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/youtube.png"))); // NOI18N
+
+        label_title.setFont(new java.awt.Font("Franklin Gothic Demi", 1, 36)); // NOI18N
+        label_title.setForeground(new java.awt.Color(255, 255, 255));
+        label_title.setText("YUTU2");
+
+        javax.swing.GroupLayout bg_cabeceraLayout = new javax.swing.GroupLayout(bg_cabecera);
+        bg_cabecera.setLayout(bg_cabeceraLayout);
+        bg_cabeceraLayout.setHorizontalGroup(
+            bg_cabeceraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bg_cabeceraLayout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(icon_logo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(label_title)
+                .addGap(124, 124, 124)
+                .addComponent(busqueda_history, javax.swing.GroupLayout.PREFERRED_SIZE, 398, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(342, Short.MAX_VALUE))
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 407, Short.MAX_VALUE)
+        bg_cabeceraLayout.setVerticalGroup(
+            bg_cabeceraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bg_cabeceraLayout.createSequentialGroup()
+                .addGroup(bg_cabeceraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(bg_cabeceraLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(icon_logo, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(bg_cabeceraLayout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addGroup(bg_cabeceraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(label_title)
+                            .addComponent(busqueda_history, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jPanel1.setBackground(new java.awt.Color(6, 137, 137));
+        background.add(bg_cabecera, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1080, 70));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+        bg_lateral_view.setBackground(new java.awt.Color(8, 63, 55));
+
+        label_inicio.setFont(new java.awt.Font("Franklin Gothic Demi", 0, 24)); // NOI18N
+        label_inicio.setForeground(new java.awt.Color(255, 255, 255));
+        label_inicio.setText("Suscripciones");
+
+        label_inicio1.setFont(new java.awt.Font("Franklin Gothic Demi", 0, 24)); // NOI18N
+        label_inicio1.setForeground(new java.awt.Color(255, 255, 255));
+        label_inicio1.setText("Historial");
+
+        label_inicio2.setFont(new java.awt.Font("Franklin Gothic Demi", 0, 24)); // NOI18N
+        label_inicio2.setForeground(new java.awt.Color(255, 255, 255));
+        label_inicio2.setText("Me gusta");
+
+        label_inicio3.setFont(new java.awt.Font("Franklin Gothic Demi", 0, 24)); // NOI18N
+        label_inicio3.setForeground(new java.awt.Color(255, 255, 255));
+        label_inicio3.setText("Inicio");
+
+        icon_home_bl.setIcon(new javax.swing.ImageIcon("C:\\Users\\brand\\OneDrive\\Documentos\\NetBeansProjects\\programacion-integrativa\\src\\main\\java\\icons\\casa (1).png")); // NOI18N
+
+        icon_suscrip_bl.setIcon(new javax.swing.ImageIcon("C:\\Users\\brand\\OneDrive\\Documentos\\NetBeansProjects\\programacion-integrativa\\src\\main\\java\\icons\\suscripcion.png")); // NOI18N
+
+        icon_history_bl.setIcon(new javax.swing.ImageIcon("C:\\Users\\brand\\OneDrive\\Documentos\\NetBeansProjects\\programacion-integrativa\\src\\main\\java\\icons\\historia.png")); // NOI18N
+
+        icon_like_bl.setIcon(new javax.swing.ImageIcon("C:\\Users\\brand\\OneDrive\\Documentos\\NetBeansProjects\\programacion-integrativa\\src\\main\\java\\icons\\like.png")); // NOI18N
+
+        icon_arrow_bl.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/flecha.png"))); // NOI18N
+
+        javax.swing.GroupLayout bg_lateral_viewLayout = new javax.swing.GroupLayout(bg_lateral_view);
+        bg_lateral_view.setLayout(bg_lateral_viewLayout);
+        bg_lateral_viewLayout.setHorizontalGroup(
+            bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bg_lateral_viewLayout.createSequentialGroup()
+                .addContainerGap(17, Short.MAX_VALUE)
+                .addGroup(bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(icon_home_bl, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(icon_suscrip_bl, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(icon_history_bl, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(icon_like_bl, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(label_inicio1)
+                    .addComponent(label_inicio3)
+                    .addGroup(bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(bg_lateral_viewLayout.createSequentialGroup()
+                            .addComponent(label_inicio2)
+                            .addGap(61, 61, 61))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bg_lateral_viewLayout.createSequentialGroup()
+                            .addComponent(label_inicio)
+                            .addGap(14, 14, 14)))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bg_lateral_viewLayout.createSequentialGroup()
+                .addComponent(jSeparator1)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bg_lateral_viewLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(icon_arrow_bl)
+                .addGap(22, 22, 22))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 74, Short.MAX_VALUE)
+        bg_lateral_viewLayout.setVerticalGroup(
+            bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(bg_lateral_viewLayout.createSequentialGroup()
+                .addGap(93, 93, 93)
+                .addComponent(icon_arrow_bl)
+                .addGap(18, 18, 18)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(label_inicio3)
+                    .addComponent(icon_home_bl))
+                .addGap(33, 33, 33)
+                .addGroup(bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(label_inicio)
+                    .addComponent(icon_suscrip_bl))
+                .addGap(27, 27, 27)
+                .addGroup(bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(label_inicio1)
+                    .addComponent(icon_history_bl))
+                .addGap(29, 29, 29)
+                .addGroup(bg_lateral_viewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(label_inicio2)
+                    .addComponent(icon_like_bl))
+                .addContainerGap(354, Short.MAX_VALUE))
         );
 
-        jPanel3.setBackground(new java.awt.Color(8, 63, 55));
-
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 141, Short.MAX_VALUE)
-        );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
+        background.add(bg_lateral_view, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -2, 220, 730));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void busqueda_historyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_busqueda_historyActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_busqueda_historyActionPerformed
 
     /**
      * @param args the command line arguments
@@ -131,8 +236,21 @@ public class Home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel background;
+    private javax.swing.JPanel bg_cabecera;
+    private javax.swing.JPanel bg_lateral_view;
+    private javax.swing.JTextField busqueda_history;
+    private javax.swing.JLabel icon_arrow_bl;
+    private javax.swing.JLabel icon_history_bl;
+    private javax.swing.JLabel icon_home_bl;
+    private javax.swing.JLabel icon_like_bl;
+    private javax.swing.JLabel icon_logo;
+    private javax.swing.JLabel icon_suscrip_bl;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel label_inicio;
+    private javax.swing.JLabel label_inicio1;
+    private javax.swing.JLabel label_inicio2;
+    private javax.swing.JLabel label_inicio3;
+    private javax.swing.JLabel label_title;
     // End of variables declaration//GEN-END:variables
 }
