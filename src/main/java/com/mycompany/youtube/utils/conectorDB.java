a5e751477da04eb3db2ce6edf5d4ceb5a6cd88d2
Brandon <al214858@alumnos.uacj.mx>
2024-03-09 18:15:17 -0700

    se refactorizo el codigo, para hacerlo mas legible y funcional

--- src/main/java/com/mycompany/youtube/utils/conectorDB.java
+++ src/main/java/com/mycompany/youtube/utils/conectorDB.java
@@ -20,35 +20,30 @@ public class conectorDB {
     private String user = "sql5688754";
     private String psw = "ZcL2VkdI5s";
 
-public conectorDB(String query) throws ClassNotFoundException{
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, user, psw);
-            
-//            Statement statement = conn.createStatement();
-//            ResultSet resultSet = statement.executeQuery(query);
-//            
-//            while (resultSet.next()) {
-//                int id = resultSet.getInt("usuario_id");
-//                String firstName = resultSet.getString("nombre_usuario");
-//                String email = resultSet.getString("email");
-//                System.out.println("ID: " + id + ", Name: " + firstName + " " + email);
-//            }
-
-            conn.close();
-        } catch (ClassNotFoundException | SQLException ex) {
-            System.err.println("¡Se produjo una excepción!");
+    public conectorDB() {
+            // Constructor vacío
         }
-    }
 
-    public void selectData() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            try (Connection conn = DriverManager.getConnection(url, user, psw)) {
-                String selectQuery = "SELECT * FROM usuario";
-                Statement statement = conn.createStatement();
-                ResultSet resultSet = statement.executeQuery(selectQuery);
-                
+        public Connection conectar() throws SQLException {
+            return DriverManager.getConnection(url, user, psw);
+        }
+
+        public void cerrarConexion(Connection conn) {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    System.err.println("Error al cerrar la conexión: " + ex.getMessage());
+                }
+            }
+        }
+
+        public void selectData() {
+            String selectQuery = "SELECT * FROM usuario";
+
+            try (Connection conn = conectar();
+                 Statement statement = conn.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectQuery)) {
                 while (resultSet.next()) {
                     int id = resultSet.getInt("usuario_id");
                     String firstName = resultSet.getString("nombre_usuario");
@@ -59,29 +54,29 @@ public conectorDB(String query) throws ClassNotFoundException{
                     System.out.println("ID: " + id + ", Nombre: " + firstName + ", Email: " + email +
                             ", Contraseña: " + contra + ", Total de videos: " + tv + ", Imagen de usuario: " + image_user);
                 }
+            } catch (SQLException ex) {
+                System.err.println("¡Se produjo una excepción al ejecutar la consulta de selección de datos!");
+                System.err.println(ex.getMessage());
             }
-        } catch (ClassNotFoundException | SQLException ex) {
-            System.err.println("¡Se produjo una excepción!");
         }
-    }
 
-    public void insertData(String nombre, String email, String contrasena, int total_video, String imagen_usuario) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            try (Connection conn = DriverManager.getConnection(url, user, psw)) {
-                String insertQuery = "INSERT INTO usuarios (nombre_usuario, email, contrasena, total_video, imagen_usuario) VALUES (?, ?, ?, ?, ?)";
-                PreparedStatement preparedStatement = conn.prepareStatement(insertQuery);
+        public void insertData(String nombre, String email, String contrasena) {
+            int total_video = 0;
+            String imagen_usuario = null;
+            String insertQuery = "INSERT INTO usuario (nombre_usuario, email, contrasena, total_video, imagen_usuario) VALUES (?, ?, ?, ?, ?)";
+
+            try (Connection conn = conectar(); 
+                 PreparedStatement preparedStatement = conn.prepareStatement(insertQuery)) {
                 preparedStatement.setString(1, nombre);
                 preparedStatement.setString(2, email);
                 preparedStatement.setString(3, contrasena);
                 preparedStatement.setInt(4, total_video);
                 preparedStatement.setString(5, imagen_usuario);
                 preparedStatement.executeUpdate();
+                System.out.println("Datos insertados correctamente.");
+            } catch (SQLException ex) {
+                System.err.println("¡Se produjo una excepción al insertar datos!");
+                System.err.println(ex.getMessage());
             }
-            System.out.println("Datos insertados correctamente.");
-        } catch (ClassNotFoundException | SQLException ex) {
-            System.err.println("¡Se produjo una excepción al insertar datos!");
-            System.err.println(ex.getMessage());
         }
     }
-}
